6f924b16386db074fd45c987f9f4e8a2b1030d14
daniil.samokhin <daniil.samokhin@sqldbm.com>
2023-04-19 08:54:26 +0400

    Uploading homework4 for lesson 2.

--- lesson3/homework5.py
+++ lesson3/homework5.py
@@ -1,6 +1,8 @@
 import json
 import os
 import csv
+import codecs
+import pickle
 
 ####!!!!! Заданиме ещё не готово полностью. Просьба, не проверять до загрузки дз в лмс.
 
@@ -9,47 +11,57 @@ import csv
 # Найдите информацию об организациях с этими ИНН в файле «traders.json»;
 # Сохраните информацию об ИНН, ОГРН и адресе организаций из файла «traders.txt» в файл «traders.csv».
 
-trInn = []
-
-f = open("traders.txt", "r")
-for i in f:
-    trInn.append(f.readline())
 
 
 
 
+def writeCsv(path):
+
+
+    f = open('traders.txt', 'r', newline='')
+    trInn = (f.readlines())
+
+    for i in trInn:
+        print(i)
+
+
+    l = open('traders.json', 'r')
+    data = json.load(l)
+
+    c = 0
+    p = 0
+
+    os.remove(path)
+    header = ['ИНН', 'ОГРН', 'Адрес']
+
+    with open(path, 'a', encoding='UTF-32', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+        file.close()
+
+    while (c <= len(trInn) - 1):
+        for i in data:
+
+            if int(i['inn']) == int(trInn[c]):
+                csvF = []
+                csvF.append(i['inn'])
+                csvF.append(i['ogrn'])
+                csvF.append(i['address'])
+                with open(path, 'a', encoding="UTF-32", newline='') as file:
+                    writer = csv.writer(file)
+                    writer.writerow(csvF)
+                    file.close()
+                p += 1
+
+        c += 1
 
 
 
-l = open("traders.json", "r")
-data = json.load(l)
+    with open(path, 'r') as file:
+        csvreader = csv.reader(codecs.open(path, 'rU', 'utf-32'))
+        for row in csvreader:
+            print(row)
 
 
 
-c = 0
-p= 0
-
-os.remove("D:/Studying/Uni/traders.csv")
-
-while(c <= len(trInn)-1):
-    for i in data:
-
-        if int(i["inn"])  == int(trInn[c]):
-            csvF = []
-            csvF.append(i["inn"])
-            csvF.append(i["ogrn"])
-            csvF.append(i["address"])
-            with open('D:/Studying/Uni/traders.csv', 'a') as file:
-                writer = csv.writer(file)
-                writer.writerow(csvF)
-
-
-            p += 1
-
-    c += 1
-
-with open("D:/Studying/Uni/traders.csv", 'r') as file:
-  csvreader = csv.reader(file)
-  for row in csvreader:
-    print(row)
-
+writeCsv('D:/Studying/Uni/traders.csv')
