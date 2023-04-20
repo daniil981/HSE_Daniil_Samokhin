9e26b54131fa32e13df689ef5d9c807c824841c2
daniil.samokhin <daniil.samokhin@sqldbm.com>
2023-04-20 13:20:42 +0400

    Uploading homework6 for lesson 3.

--- lesson3/homework6.py
+++ lesson3/homework6.py
@@ -1,12 +1,11 @@
 from ics import Calendar, Event
 import json
-
-#Задание не завершено
+import re
 
 
 
 #'C:/Users/samox/Downloads/А82-6711-2023.ics'
-path = 'C:/Users/samox/Downloads/А82-6711-2023.ics'
+path = 'C:/Users/samox/Downloads/А56-32581-2023.ics'
 
 
 
@@ -16,26 +15,31 @@ path = 'C:/Users/samox/Downloads/А82-6711-2023.ics'
 def parseCrt(pth):
     with open(pth, 'r', encoding="utf8") as mf:
         fm = mf.read()
-
-    c = Calendar(fm)
+        c = Calendar(fm)
 
     json_string = {}
     dict(json_string)
 
     for e in c.events:
-        json_string["case_number"] = e.name
-        json_string["start"] = str(e.begin)
-        json_string["end"] = str(e.end)
-        json_string["location"] = e.location
-        json_string["description"] = e.description
+        pat = r'(А\d+.*\d)'
+        cnm = re.findall(pat,  e.name)
+        if str(e.begin) != '0001-01-01T00:00:00+00:00' and str(e.end) != '0001-01-01T00:00:00+00:00' and e.location != 'None':
+            json_string["case_number"] = cnm[0]
+            json_string["start"] = str(e.begin)
+            json_string["end"] = str(e.end)
+            json_string["location"] = e.location
+            json_string["description"] = e.description
 
-    print(type(json_string))
+            with open('json_data.json', 'w', encoding="utf8") as outfile:
+                json.dump(json_string, outfile)
 
-    for i in json_string:
-        print(json_string)
+        else:
+            print("Дело не существует, пожалуйста, загрузите другой файл")
 
-    with open('json_data.json', 'w', encoding="utf8") as outfile:
-        json.dump(json_string, outfile)
+
+
+
 
 
 parseCrt(path)
+
